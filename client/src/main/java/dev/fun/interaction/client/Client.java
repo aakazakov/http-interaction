31b3c42916cf26bf834059445390c900e9def2e2
nito nise <aakazakovv@yandex.ru>
2021-02-19 16:49:46 +0300

    init

---
+++ client/src/main/java/dev/fun/interaction/client/Client.java
@@ -0,0 +1,9 @@
+package dev.fun.interaction.client;
+
+public class Client {
+	
+	public static void main(String[] args) {
+		System.out.println("client");
+	}
+	
+}
---
+++ server/src/test/java/dev/fun/interaction/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fun.interaction.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
