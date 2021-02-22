0815923315bc0e9cb2b897b8a05264583cdde99e
nito nise <aakazakovv@yandex.ru>
2021-02-22 15:09:56 +0300

    refactor

--- client/src/main/java/dev/fun/interaction/client/Launch.java
+++ client/src/main/java/dev/fun/interaction/client/Launch.java
@@ -3,13 +3,47 @@ package dev.fun.interaction.client;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class Launch {
 	
 	public static void main(String[] args) {
 		
-		try (Socket socket = new Socket("localhost", 8089)) {
+		get();
+		post();
+		
+	}
+	
+	static void get() {
+		try (Socket socket = new Socket("localhost", 8089);
+				InputStream in = new BufferedInputStream(socket.getInputStream());
+				OutputStream out = socket.getOutputStream();) {
+			
+			StringBuilder req = new StringBuilder();
+			
+			req.append("GET /req/get/hello HTTP/1.1").append("\r\n");
+			req.append("Host: ").append("localhost:8089").append("\r\n");
+			req.append("Accept: ").append("application/json;charset=UTF-8").append("\r\n");
+			req.append("Connection: ").append("close").append("\r\n");
+			req.append("\r\n");
+			
+			out.write(req.toString().getBytes());
+			
+			int x;
+			while((x = in.read()) != -1) {
+				System.out.print((char) x);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	static void post() {
+		try (Socket socket = new Socket("localhost", 8089);
+				InputStream in = new BufferedInputStream(socket.getInputStream());
+				OutputStream out = socket.getOutputStream();) {
 			
 			StringBuilder req = new StringBuilder();
 			
@@ -24,46 +58,16 @@ public class Launch {
 			req.append("\r\n");
 			req.append(payload);
 			
-			socket.getOutputStream().write(req.toString().getBytes());
+			out.write(req.toString().getBytes());
 			
-			InputStream in = new BufferedInputStream(socket.getInputStream());
 			int x;
 			while((x = in.read()) != -1) {
 				System.out.print((char) x);
 			}
-			
-			in.close();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	}
-	
-	static void get() {
-		try (Socket socket = new Socket("localhost", 8089)) {
-			
-			StringBuilder req = new StringBuilder();
-			
-			req.append("GET /req/get/hello HTTP/1.1").append("\r\n");
-			req.append("Host: ").append("localhost:8089").append("\r\n");
-			req.append("Accept: ").append("application/json;charset=UTF-8").append("\r\n");
-			req.append("Connection: ").append("close").append("\r\n");
-			req.append("\r\n");
-			
-			socket.getOutputStream().write(req.toString().getBytes());
-			
-			InputStream in = new BufferedInputStream(socket.getInputStream());
-			int x;
-			while((x = in.read()) != -1) {
-				System.out.print((char) x);
-			}
-			
-			in.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
 	}
 	
 }
