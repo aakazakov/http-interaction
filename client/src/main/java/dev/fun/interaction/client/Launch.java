95ad92c86bca2b3a5ba8f7699df32d8686f977b5
nito nise <aakazakovv@yandex.ru>
2021-02-22 14:53:03 +0300

    add post

--- client/src/main/java/dev/fun/interaction/client/Launch.java
+++ client/src/main/java/dev/fun/interaction/client/Launch.java
@@ -13,11 +13,42 @@ public class Launch {
 			
 			StringBuilder req = new StringBuilder();
 			
-			req.append("GET /req/hello HTTP/1.1").append("\r\n");
+			String payload = "{\n\t\t\t\t\"value\": \"world\"\n}";
+			
+			req.append("POST /req/post HTTP/1.1").append("\r\n");
 			req.append("Host: ").append("localhost:8089").append("\r\n");
-			req.append("Accept: ").append("text/plain;charset=UTF-8").append("\r\n");
+			req.append("Accept: ").append("application/json;charset=UTF-8").append("\r\n");
 			req.append("Connection: ").append("close").append("\r\n");
-			req.append("Content-Type: ").append("text/plain;charset=UTF-8").append("\r\n");
+			req.append("Content-Type: ").append("application/json;charset=UTF-8").append("\r\n");
+			req.append("Content-Length: ").append(payload.getBytes().length).append("\r\n");
+			req.append("\r\n");
+			req.append(payload);
+			
+			socket.getOutputStream().write(req.toString().getBytes());
+			
+			InputStream in = new BufferedInputStream(socket.getInputStream());
+			int x;
+			while((x = in.read()) != -1) {
+				System.out.print((char) x);
+			}
+			
+			in.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	static void get() {
+		try (Socket socket = new Socket("localhost", 8089)) {
+			
+			StringBuilder req = new StringBuilder();
+			
+			req.append("GET /req/get/hello HTTP/1.1").append("\r\n");
+			req.append("Host: ").append("localhost:8089").append("\r\n");
+			req.append("Accept: ").append("application/json;charset=UTF-8").append("\r\n");
+			req.append("Connection: ").append("close").append("\r\n");
 			req.append("\r\n");
 			
 			socket.getOutputStream().write(req.toString().getBytes());
@@ -28,10 +59,11 @@ public class Launch {
 				System.out.print((char) x);
 			}
 			
+			in.close();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
+		}		
 	}
 	
 }
--- server/src/main/java/dev/fun/interaction/server/controller/RequestController.java
+++ server/src/main/java/dev/fun/interaction/server/controller/RequestController.java
@@ -1,19 +1,35 @@
 package dev.fun.interaction.server.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.fun.interaction.server.dto.SimpleDto;
+
 @RestController
-@RequestMapping(name ="/", produces = MediaType.TEXT_PLAIN_VALUE)
+@RequestMapping(name ="/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RequestController {
 
-	@GetMapping("/{var}")
-	public String getResponse(@PathVariable(name = "var") String var) {
+	@GetMapping("/get/{var}")
+	public Map<String, String> getResponse(@PathVariable(name = "var") String var) {
 		System.out.println("I've got it");
-		return "echo: " + var + "\n";
+		Map<String, String> payload = new HashMap<>();
+		payload.put("echo", var);
+		return payload;
+	}
+	
+	@PostMapping(path = "/post", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public SimpleDto postResponse(@RequestBody SimpleDto dto) {
+		System.out.println("I've got it");
+		dto.setValue(dto.getValue() + " ))");
+		return dto;
 	}
 	
 }
