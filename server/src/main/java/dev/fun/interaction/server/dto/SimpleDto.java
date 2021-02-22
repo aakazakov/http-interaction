dae60b63dd522d46e04a009511f01615bd9271ad
nito nise <aakazakovv@yandex.ru>
2021-02-22 14:52:12 +0300

    add dto

---
+++ server/src/main/java/dev/fun/interaction/server/dto/SimpleDto.java
@@ -0,0 +1,15 @@
+package dev.fun.interaction.server.dto;
+
+public class SimpleDto {
+	
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
